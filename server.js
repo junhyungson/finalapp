b7817b0e4fd67efe99cccfc1a85f11a7ee231f15
Jun Son <junhyungson89@gmail.com>
2019-04-17 06:52:37 -0700

    weather working

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const port = process.env.PORT || 8080;
 const from_api = require('./challengeapi1.js');
 var app = express();
-
+const request = require('request');
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -29,9 +29,57 @@ var getWeather = async function()
         result = error;
     }
 }
-app.get('/weather', async (request, response)=> {
-    getWeather();
-console.log(result);
+var result='';
+var country = 'canada'
+var getcapital = ((country) => {
+    return new Promise((resolve, reject) =>{
+        request({
+            // https://restcountries.eu/rest/v2/name/canada?fullText=true
+            url: `https://restcountries.eu/rest/v2/name/` + encodeURIComponent(country) + `?fullText=true`,
+            json: true
+        }, (error, response, body) => {
+            // console.log(body)
+            if(error) {
+                reject(error);
+            }
+            else if (body.status == '404')
+            {
+                reject(body.message);
+            }
+            
+            else
+            {
+                resolve(body[0].capital);
+            }
+        });
+    });
+});
+
+var getWeather = ((city, country) => {
+    return new Promise((resolve, reject) => {
+        request({
+            // https://api.openweathermap.org/data/2.5/weather?q=ottawa,canada&appid=2264e3b73dc094131aeb3adfa3d71b61
+            url: `https://api.openweathermap.org/data/2.5/weather?q=`+ encodeURIComponent(city) +',' + encodeURIComponent(country) + `&appid=2264e3b73dc094131aeb3adfa3d71b61`,
+            json:true
+        }, (error, response, body) => {
+            resolve({
+                temp: body.main.temp,
+                wind: body.wind.speed
+                
+            });
+        });
+    });
+})
+getcapital(country).then((capital) =>{
+    getWeather(capital, country).then((weather) =>{
+        result = `the weather in ${capital}, capital of ${country} is 
+        ${JSON.stringify(weather.temp)} with wind speed of ${JSON.stringify(weather.wind)}`;
+    })
+}).catch((error) =>{
+    result = error;
+})
+app.get('/weather', (request, response)=> {
+    // getWeather();
     response.render('weather.hbs', {
         weather: result
     });
