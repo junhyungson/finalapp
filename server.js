55417cbf1187ceecbab9d26830cd4c442bb1239c
Jun Son <junhyungson89@gmail.com>
2019-04-16 22:03:49 -0700

    add weather api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 8080;
+var from_api = require('from')
 var app = express();
 
 
