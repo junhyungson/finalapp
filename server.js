85ffa20a9e926e718b54a2988edc12c9015b386c
Jun Son <junhyungson89@gmail.com>
2019-04-16 20:59:09 -0700

    adding packages

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 8080;
 var app = express();
 
 app.get('/', (request, response) => {
@@ -8,4 +10,6 @@ app.get('/', (request, response) => {
 app.get('/info', (request, response) => {
     response.send('my info page');
 })
-app.listen(8080);
+app.listen(port, () => {
+    console.log(`server is up on the port ${port}`);
+});
