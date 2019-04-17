f4de3e92d733586958ca797ce8761fd16e97a546
Jun Son <junhyungson89@gmail.com>
2019-04-16 21:59:55 -0700

    add weather

--- server.js
+++ server.js
@@ -4,9 +4,38 @@ const fs = require('fs');
 const port = process.env.PORT || 8080;
 var app = express();
 
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (request, response) => {
-    response.send('welcome page');
+    response.render('main.hbs');
 });
+
+var result='';
+var country = 'canada'
+var errormessage = ''
+var getWeather = async function()
+{
+    try{
+        capitalResult =  await from_api.getcapital(country);
+        weatherResult = await from_api.getWeather(capitalResult,country);
+        result = `the weather in ${capitalResult}, capital of ${country} is 
+        ${JSON.stringify(Weatherresult.temp)} with wind speed of ${JSON.stringify(Weatherresult.wind)}`;
+    }
+    catch(error) {
+        result = error;
+    }
+}
+app.get('/weather', async (request, response)=> {
+    getWeather();
+// console.log(result);
+    response.render('weather.hbs', {
+        weather: result
+    });
+});
+
 app.get('/info', (request, response) => {
     response.send('my info page');
 });
